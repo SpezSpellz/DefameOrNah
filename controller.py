509221dcfcb0104ae6c5cefeb94117d14467f261
capthehacker99 <pawat.capsule@gmail.com>
2025-04-15 10:48:45 +0700

    feat: add message limit of 4096 characters

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ import joblib
 from ollama import AsyncClient
 import pandas as pd
 from decouple import config
+from fastapi import HTTPException
 
 model = joblib.load(config("MODEL_PATH", default="../analytics/random_forest.joblib"))
 data_combined = pd.read_csv(config("DATA_PATH", default="../analytics/data_combined.csv"))
@@ -16,8 +17,12 @@ from stub.models import (
     MessageLengthFrequencyEntry
 )
 
+MAX_MESSAGE_LENGTH = 4096
+
 async def check_message(message: str) -> DefamationInferenceResponse:
     """API to check whether message is defamation or not"""
+    if len(message) > MAX_MESSAGE_LENGTH:
+        raise HTTPException(status_code=400, detail="Parameter `message` must be less than 4096 bytes long")
     message_log.write(message.replace("\n", "").replace("\r", "") + "\n")
     message_log.flush()
     embeddings = (await AsyncClient().embed(model="bge-m3", input=[message]))["embeddings"]
